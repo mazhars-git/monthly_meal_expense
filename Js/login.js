231a2d8db19d6a5dd5b347e1a0a472616771625e
Md. Mazharul Islam <mazharulislam611@gmail.com>
2023-03-10 19:34:16 +0600

    new page created

--- Js/login.js
+++ Js/login.js
@@ -7,5 +7,11 @@ document.getElementById("btn-submit").addEventListener('click', function(){
     const userPassword = document.getElementById('user-password');
     const password = userPassword.value;
 
-    console.log('User:', email, password)
+    //check valid or invalid
+    if (email === 'amarbank@gmail.com' && password === '1234') {
+        window.location.href = 'home.html';
+    }else{
+        console.log('Invalid User')
+    }
+       
 })
