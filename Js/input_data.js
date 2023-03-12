d0b30fcfe74f167b3d180b15b2e29d1375dcb29e
Md. Mazharul Islam <mazharulislam611@gmail.com>
2023-03-13 00:46:33 +0600

    set name

--- Js/input_data.js
+++ Js/input_data.js
@@ -2,6 +2,15 @@ document.getElementById('submit-data').addEventListener('click', function (e) {
     e.preventDefault();
     console.log('clicked in data submission')
     const name = document.getElementById('user-name');
-    
+    const user = name.value;
+
+
+    //get user name
+
+    const userName = document.getElementById('set-name');
+    const userField = userName.innerText;
+    userName.innerText = user;
+
+    console.log()
 
 })
