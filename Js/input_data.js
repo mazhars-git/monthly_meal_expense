615f387dc9e8f10d240728ba31f56f597ea7904f
Md. Mazharul Islam <mazharulislam611@gmail.com>
2023-03-12 23:59:12 +0600

    update

--- Js/input_data.js
+++ Js/input_data.js
@@ -1,4 +1,7 @@
 document.getElementById('submit-data').addEventListener('click', function (e) {
     e.preventDefault();
     console.log('clicked in data submission')
+    const name = document.getElementById('user-name');
+    
+
 })
